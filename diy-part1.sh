9d5a4c54f2e7dcbf7d0f86a5ad77287dd9d201ea
HOT-POT1 <954905323@QQ.COM>
2021-08-27 19:33:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,12 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+cd openwrt/package
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
+git clone https://github.com/VPN-V2Ray/luci-theme-edge
+git clone https://github.com/tty228/luci-app-serverchan
+svn co https://github.com/281677160/openwrt-package/trunk/luci-app-socat
+git clone https://github.com/jerrykuku/luci-app-argon-config
+cd lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
